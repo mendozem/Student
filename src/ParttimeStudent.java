3e10cd84bd6786e48c92efe9ce497e3bc33423b0
mendozem <mendozem@sheridancollege.ca>
2026-05-25 10:17:53 -0400

    branch - new class added

---
+++ src/ParttimeStudent.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author vegan
+ */
+public class ParttimeStudent {
+    private int semester;
+
+    /**
+     * @return the semester
+     */
+    public int getSemester() {
+        return semester;
+    }
+
+    /**
+     * @param semester the semester to set
+     */
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+}
